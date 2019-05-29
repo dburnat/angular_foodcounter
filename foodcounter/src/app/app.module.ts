74ddef91e94e9c5909966591a7598cdf8bc77d1e
dburnat <d.burnat97@gmail.com>
2019-05-29 22:07:19 +0200

    added two forms

--- foodcounter/src/app/app.module.ts
+++ foodcounter/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatInputModule, MatCheckboxModule, MatSelectModule , MatChipsModule, MatSliderModule, MatCardModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserDetailComponent } from './user-detail/user-detail.component';
 import { HomeComponent } from './home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,15 @@ import { HomeComponent } from './home/home.component';
     MatIconModule,
     MatListModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatSliderModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- foodcounter/src/app/user-detail/user-detail.component.ts
+++ foodcounter/src/app/user-detail/user-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms'
 
 @Component({
   selector: 'app-user-detail',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDetailComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup;
+  loginForm: FormGroup
+
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.myForm = this.fb.group({
+      firstName: '',
+      lastName: '',
+      weight: ['',[ Validators.required] ],
+      goalWeight: ['',[ Validators.required] ],
+      dailyCalories: ['',[ Validators.required] ],
+    });
+    this.loginForm = this.fb.group({
+      login: '',
+      password: '',
+      email: ''
+    })
+  }
+
+  get weight(){
+    return this.myForm.get('weight');
+  }
+  get goalWeight(){
+    return this.myForm.get('goalWeight');
+  }
+  get dailyCalories(){
+    return this.myForm.get('dailyCalories');
   }
 
 }
